4abc7149a1849165f0c9166700716ab14b682fcc
yangkun <1269769296@qq.com>
2021-02-18 17:42:42 +0800

    add line success. ctrl + middle scroll to expend drone

--- RflySim/rflyshow.py
+++ RflySim/rflyshow.py
@@ -33,10 +33,27 @@ class RFly:
         self.mav.sendUE4Cmd(b'RflyCameraPosAng 0.3 0 0.05 0 0 0',0)
         time.sleep(0.5)
 
-    def render(self, WL_positions, TL_positions):
+    def draw_line(self, id, start, end):
+        start = np.array([start.position[0], start.position[1], -start.position[2]])
+        end = np.array([end.position[0], end.position[1], -end.position[2]])
+
+        PosE = (start + end) / 2
+        AngEuler = [0, 
+                    np.arctan2(-end[2] + start[2], np.linalg.norm([end[0] - start[0], end[1] - start[1]])), 
+                    np.arctan2(end[1] - start[1], end[0] - start[0])]
+        Scale = [np.linalg.norm(start - end), 0.1, 0.1]
+        print("PosE", PosE)
+        print("AngEuler", AngEuler)
+        print("Scale", Scale)
+        self.mav.sendUE4PosScale(id, 207, 0, PosE, AngEuler, Scale)
+
+    def render(self, WL_positions, TL_positions, results):
         print("WL_positions", WL_positions)
         for i in range(len(WL_positions)):
-            self.mav.sendUE4Pos(i, 3, 0, [WL_positions[i][0],WL_positions[i][1],-WL_positions[i][2]], [0,0,0])
+            self.mav.sendUE4Pos(i, 3, 0, WL_positions[i], [0,0,0])
         print("TL_positions", TL_positions)
         for i in range(len(TL_positions)):
-            self.mav.sendUE4Pos(100+i, 5, 0, [TL_positions[i][0],TL_positions[i][1],-TL_positions[i][2]], [0,0,0])
+            self.mav.sendUE4Pos(100+i, 5, 0, TL_positions[i], [0,0,0])
+        for i in range(len(results)):
+            self.draw_line(200+i, results[i][0], results[i][1])
+
--- plot.py
+++ plot.py
@@ -63,4 +63,6 @@ class Theater:
         # self.ax.set_ylim(-50, 100)
         self.fig.savefig("1.png", dpi=600)
         plt.show()
-        self.rfly.render(self.gta.WL.getPositionsList(), self.gta.TL.getPositionsList())
+        WL_positions = self.gta.WL.getPositionsList()
+        TL_positions = self.gta.TL.getPositionsList()
+        self.rfly.render([[w[0], w[1], -w[2]] for w in WL_positions], [[t[0], t[1], -t[2]] for t in TL_positions], self.gta.algs.tree_result)
